503980e9066250133d57687d1ce22c829fca20a0
DVSneuro <david.v.smith@temple.edu>
2019-01-28 15:43:27 -0500

    revise for rutgers data

--- code/gen_3col_files.sh
+++ code/gen_3col_files.sh
@@ -1,44 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# make everything relative to root of the GitHub repository
+cd ..
 maindir=`pwd`
 baseout=${maindir}/fsl/EVfiles
 if [ ! -d ${baseout} ]; then
-  mkdir -p $baseout
+	mkdir -p $baseout
 fi
 
 sub=$1
-nruns=$2
+nruns=`ls -1 ${maindir}/ds000000/sub-${sub}/func/sub-${sub}_task-cardgame_run-0?_events.tsv | wc -l`
 
 for run in `seq $nruns`; do
-  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-trust_run-0${run}_events.tsv
-  output=${baseout}/sub-${sub}/trust
-  mkdir -p $output
-  if [ -e $input ]; then
-    bash BIDSto3col.sh $input ${output}/run-0${run}
-  else
-    echo "PATH ERROR: cannot locate ${input}."
-    exit
-  fi
+	bartfile=${maindir}/sourcedata/bids_bart/sub-${sub}/func/sub-${sub}_task-cardgame_run-0${run}_events.tsv
+	if [ -e $bartfile ]; then
+		nlines=`cat $bartfile | wc -l`
+		if [ $nlines -gt 1 ]; then
+			# replace HeuDiConv *.tsv with real *.tsv file
+			input=${maindir}/ds000000/sub-${sub}/func/sub-${sub}_task-cardgame_run-0${run}_events.tsv
+			cp $bartfile ${input}
+			output=${baseout}/sub-${sub}
+			mkdir -p $output
+			if [ -e $input ]; then
+				bash ${maindir}/code/BIDSto3col.sh $input ${output}/run-0${run}
+			else
+				echo "PATH ERROR: cannot locate ${input}"
+				exit
+			fi
+		else
+			#echo "not enough lines: ${bartfile}"
+			echo "not enough lines: ${bartfile}" >> badformat_tsv.log
+		fi
+	else
+		#echo "missing file: ${bartfile}"
+		echo "missing file: ${bartfile}" >> missing_tsv.log
+	fi
 done
-for run in 1 2; do
-  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-sharedreward_run-0${run}_events.tsv
-  output=${baseout}/sub-${sub}/sharedreward
-  mkdir -p $output
-  if [ -e $input ]; then
-    bash BIDSto3col.sh $input ${output}/run-0${run}
-  else
-    echo "PATH ERROR: cannot locate ${input}."
-    exit
-  fi
-done
-for run in 1 2; do
-  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-ultimatum_run-0${run}_events.tsv
-  output=${baseout}/sub-${sub}/ultimatum
-  mkdir -p $output
-  if [ -e $input ]; then
-    bash BIDSto3col.sh $input ${output}/run-0${run}
-  else
-    echo "PATH ERROR: cannot locate ${input}."
-    exit
-  fi
-done
+
--- code/runall_gen_3col_files.sh
+++ code/runall_gen_3col_files.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
+# leaving out 210 for now
+# skipping 179 (pilot subject)
+for sub in 180 189 203 207 208 209 211 212 213 214 215 217; do
 
-for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5"; do
-
-  set -- $subrun
-  sub=$1
-  run=$2
-  echo "$sub $run"
-
-	#Manages the number of jobs and cores
-	SCRIPTNAME=gen_3col_files.sh
-	NCORES=12
-	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-  		sleep 1m
-	done
-	bash $SCRIPTNAME $sub $run
-	sleep 5s
+	bash gen_3col_files.sh $sub
 
 done
