740d78c889a721a2101e9290816f055edb471051
DVSneuro <david.v.smith@temple.edu>
2019-02-09 14:47:01 -0500

    initial commit

--- code/runall_L1stats.sh
+++ code/runall_L1stats.sh
@@ -1,45 +1,25 @@
 #!/bin/bash
 
 
-for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5"; do
+cat runcount.tsv | 
+while read a; do 
+	set -- $a
+	sub=$1
+	nruns=$2
 
-  set -- $subrun
-  sub=$1
-  nruns=$2
-  #echo "$sub $nruns"
-
-  for run in `seq $nruns`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-trust_model-01.sh
-  	NCORES=16
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-
-  for run in `seq 2`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-sharedreward_model-01.sh
-  	NCORES=5
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-  
-  for run in `seq 2`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-ultimatum_model-01.sh
-  	NCORES=5
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-
-
+	echo "$sub $nruns"
+		
+	for run in `seq $nruns`; do
+	
+	  	#Manages the number of jobs and cores
+	  	SCRIPTNAME=L1_task-reward_model-01.sh
+	  	NCORES=32
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	 		sleep 1s
+	  	done
+	  	bash $SCRIPTNAME $sub $run &
+	  	sleep 5s
+  	
+	done
+		
 done
