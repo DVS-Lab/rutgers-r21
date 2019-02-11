4ffaccbcc79f2dee78007f10949929260758b6d7
DVSneuro <david.v.smith@temple.edu>
2019-02-11 00:43:54 -0500

    rename

--- code/extractROI.sh
+++ code/extractROI.sh
@@ -4,18 +4,20 @@
 cd ..
 maindir=`pwd`
 
-MASK=${maindir}/masks/NAcc_resliced.nii.gz
-TASK=cardgame
-TYPE=act
+ROI=$1
 
-for COPENUM in 1 2 3 4; do
+MASK=${maindir}/masks/${ROI}.nii.gz #NAcc_resliced or PreCun_func
+TASK=cardgame
+TYPE=ppi
+
+for COPENUM in 11 12 13 14; do
 	
 	cnum_padded=`zeropad ${COPENUM} 2`
 	
 	MAINOUTPUT=${maindir}/derivatives/fsl/L3_n14_model-01
 	OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_model-01_type-${TYPE}_cope-${cnum_padded}
 	DATA=${OUTPUT}.gfeat/cope1.feat/filtered_func_data.nii.gz
-	fslmeants -i $DATA -o NAcc_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+	fslmeants -i $DATA -o ${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
 
 
 done
