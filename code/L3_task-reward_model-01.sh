c0a2d1021ef98677046c75352f25108a6e81c283
DVSneuro <david.v.smith@temple.edu>
2019-04-24 22:21:59 -0400

    minor updates

--- code/L3_task-reward_model-01.sh
+++ code/L3_task-reward_model-01.sh
@@ -14,7 +14,7 @@ for COPENUM in `seq $ncopes`; do
 	cnum_padded=`zeropad ${COPENUM} 2`
 	
 	# delete incomplete output
-	MAINOUTPUT=${maindir}/derivatives/fsl/L3_n30_model-01_FLAME1+2
+	MAINOUTPUT=${maindir}/derivatives/fsl/L3_n31_model-01_FLAME1+2
 	if [ ! -d ${MAINOUTPUT} ]; then
 		mkdir $MAINOUTPUT
 	fi
@@ -25,7 +25,39 @@ for COPENUM in `seq $ncopes`; do
 		rm -rf ${OUTPUT}.gfeat
 	fi
 	
-	ITEMPLATE=${maindir}/code/templates/L3_template_n30.fsf
+	ITEMPLATE=${maindir}/code/templates/L3_template_n31.fsf
+	OTEMPLATE=${MAINOUTPUT}/L3_task-${TASK}_model-01_type-${TYPE}_cope-${cnum_padded}.fsf
+	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	-e 's@TYPE@'$TYPE'@g' \
+	-e 's@COPENUM@'$COPENUM'@g' \
+	<$ITEMPLATE> $OTEMPLATE
+	
+	# runs feat on output template
+	feat $OTEMPLATE &
+	sleep 1
+done
+
+
+ncopes=9
+TYPE=act
+
+for COPENUM in `seq $ncopes`; do
+	
+	cnum_padded=`zeropad ${COPENUM} 2`
+	
+	# delete incomplete output
+	MAINOUTPUT=${maindir}/derivatives/fsl/L3_n31_model-01_FLAME1+2
+	if [ ! -d ${MAINOUTPUT} ]; then
+		mkdir $MAINOUTPUT
+	fi
+	OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_model-01_type-${TYPE}_cope-${cnum_padded}
+	if [ -e ${OUTPUT}.gfeat/cope${ncopes}.feat/cluster_mask_zstat1.nii.gz ]; then
+		exit
+	else
+		rm -rf ${OUTPUT}.gfeat
+	fi
+	
+	ITEMPLATE=${maindir}/code/templates/L3_template_n31.fsf
 	OTEMPLATE=${MAINOUTPUT}/L3_task-${TASK}_model-01_type-${TYPE}_cope-${cnum_padded}.fsf
 	sed -e 's@OUTPUT@'$OUTPUT'@g' \
 	-e 's@TYPE@'$TYPE'@g' \
