5d0b5b3b17508eb6cb435e03ee745f68fa5c4d5d
DVSneuro <david.v.smith@temple.edu>
2019-04-26 13:17:15 -0400

    update for n=27, exclusions

--- code/prelimROIanalyses.m
+++ code/prelimROIanalyses.m
@@ -1,4 +1,5 @@
 clear; close all;
+
 c1 = load('PreCun_func_type-ppi_cope-11.txt');
 c2 = load('PreCun_func_type-ppi_cope-12.txt');
 c3 = load('PreCun_func_type-ppi_cope-13.txt');
@@ -11,10 +12,10 @@ axis square
 print -depsc conn_PreCun-NAcc
 
 
-c1 = load('NAcc_type-act_cope-01.txt');
-c2 = load('NAcc_type-act_cope-02.txt');
-c3 = load('NAcc_type-act_cope-03.txt');
-c4 = load('NAcc_type-act_cope-04.txt');
+c1 = load('NAcc_resliced_type-act_cope-01.txt');
+c2 = load('NAcc_resliced_type-act_cope-02.txt');
+c3 = load('NAcc_resliced_type-act_cope-03.txt');
+c4 = load('NAcc_resliced_type-act_cope-04.txt');
 
 reward = [c1 c2];
 punish = [c3 c4];
