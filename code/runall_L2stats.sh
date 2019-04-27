18e16120b915e2e712c525815debc08152c6d252
DVSneuro <david.v.smith@temple.edu>
2019-04-27 10:20:39 -0400

    add nppi

--- code/runall_L2stats.sh
+++ code/runall_L2stats.sh
@@ -3,24 +3,28 @@
 
 rm -rf ../check_L2.log
 
-cat runcount.tsv | 
-while read a; do 
-	set -- $a
-	sub=$1
-	nruns=$2
-	if [ $sub -eq 224 ]; then
-		continue
-	fi
+for ppi in dmn ecn; do
 
-  	#Manages the number of jobs and cores
-  	#SCRIPTNAME=L2_task-reward_model-01.sh
-  	SCRIPTNAME=L2_task-reward_model-01_ppi.sh
-  	NCORES=32
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
- 		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $nruns &
-  	sleep 5s
-  	
-		
+	cat runcount.tsv | 
+	while read a; do 
+		set -- $a
+		sub=$1
+		nruns=$2
+		if [ $sub -eq 224 ]; then
+			continue
+		fi
+	
+	  	#Manages the number of jobs and cores
+	  	#SCRIPTNAME=L2_task-reward_model-01.sh
+	  	#SCRIPTNAME=L2_task-reward_model-01_ppi.sh
+	  	SCRIPTNAME=L2_task-reward_model-01_nppi.sh
+	  	NCORES=32
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	 		sleep 1s
+	  	done
+	  	bash $SCRIPTNAME $sub $nruns $ppi &
+	  	sleep 5s
+	  	
+			
+	done
 done
